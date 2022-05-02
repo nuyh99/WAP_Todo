9aa525da22285c1fb67a4d3ea44d8a20be27bc7e
jaehyun hwang <93072571+nuyh99@users.noreply.github.com>
2022-05-02 15:38:05 +0900

    commit

---
+++ src/test/java/Wap/Todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package Wap.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
