3a7e76698e5b54d3c75a4c1bfbaeb913fcccc96d
Jeong Seok <wjdtjr8649@naver.com>
2022-05-04 12:11:12 +0900

    05-04 Add Index

--- src/RoomDetail/RoomDetail.js
+++ src/RoomDetail/RoomDetail.js
@@ -5,15 +5,27 @@ import AddTaskIcon from "@mui/icons-material/AddTask";
 import { Button, Input, TextField } from "@mui/material";
 import PublishIcon from "@mui/icons-material/Publish";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { DesignServices } from "@mui/icons-material";
 
 const itemsFromBackend = [
-  { id: uuid(), content: "알고리즘 과제", condition: "ready" },
-  { id: uuid(), content: "운영체제 과제", condition: "processing" },
-  { id: uuid(), content: "중간고사", condition: "done" },
-  { id: uuid(), content: "기말고사", condition: "ready" },
-  { id: uuid(), content: "팀프로젝트", condition: "ready" },
-  { id: uuid(), content: "팀프로젝트 중간발표", condition: "processing" },
-  { id: uuid(), content: "팀프로젝트 최종발표", condition: "defer" },
+  { id: uuid(), content: "알고리즘 과제", condition: "ready", index: 0 },
+  { id: uuid(), content: "운영체제 과제", condition: "processing", index: 0 },
+  { id: uuid(), content: "중간고사", condition: "done", index: 0 },
+  { id: uuid(), content: "기말고사", condition: "ready", index: 1 },
+  { id: uuid(), content: "팀프로젝트", condition: "ready", index: 2 },
+  {
+    id: uuid(),
+    content: "팀프로젝트 중간발표",
+    condition: "processing",
+    index: 1,
+  },
+  { id: uuid(), content: "팀프로젝트 최종발표", condition: "defer", index: 0 },
+  {
+    id: uuid(),
+    content: "스포츠데이터의이해 과제",
+    condition: "ready",
+    index: 3,
+  },
 ];
 
 const columnsFromBackend = {
@@ -47,15 +59,31 @@ const onDragEnd = (result, columns, setColumns) => {
     const sourceColumn = columns[source.droppableId];
     const destColumn = columns[destination.droppableId];
 
-    console.log(destColumn);
-
     const sourceItems = [...sourceColumn.items];
     const destItems = [...destColumn.items];
     const [removed] = sourceItems.splice(source.index, 1);
-    // Column이 다르면 condition을 변경
-    removed.condition = destination.droppableId;
 
+    // sourceItem의 index 재 정렬
+    sourceItems.forEach((element) => {
+      if (element.index > source.index) {
+        element.index -= 1;
+      }
+    });
+
+    // Column이 다르면 condition과 index를 변경
+    removed.condition = destination.droppableId;
+    // 옮기는 Item의 항목의 index를 destination의 index로 변경
+    removed.index = destination.index;
+    // destination의 index를 재 정렬
+    destItems.forEach((element) => {
+      if (element.index >= destination.index) {
+        element.index += 1;
+      }
+    });
     destItems.splice(destination.index, 0, removed);
+
+    console.log(sourceItems);
+    console.log(destItems);
 
     setColumns({
       ...columns,
@@ -69,10 +97,27 @@ const onDragEnd = (result, columns, setColumns) => {
       },
     });
   } else {
+    // 같은 Droppable에서 바꿀 때
     const column = columns[source.droppableId];
     const copiedItems = [...column.items];
     const [removed] = copiedItems.splice(source.index, 1);
+
+    removed.index = destination.index;
+
     copiedItems.splice(destination.index, 0, removed);
+
+    /*
+    전체 index 재정렬아닌 바뀐 항목만 index 재정렬하도록 수정하기
+    */
+
+    // Droppable Item의 Index 재정렬
+    let test = 0;
+    copiedItems.forEach((element) => {
+      element.index = test;
+      test++;
+    });
+
+    console.log(copiedItems);
     setColumns({
       ...columns,
       [source.droppableId]: {
@@ -85,35 +130,40 @@ const onDragEnd = (result, columns, setColumns) => {
 
 function RoomDetail() {
   const [columns, setColumns] = useState(columnsFromBackend);
+
   const [isClick, setIsClick] = useState(false);
 
   const [toDoAdd, setToDoAdd] = useState({
     content: "",
+    index: 0,
   });
 
-  // 일정추가 Input 함수
+  console.log(columns);
+
+  // 일정 추가하기 및 취소 버튼
+  const onClickAddOpen = () => {
+    setIsClick((prev) => !prev);
+    setToDoAdd({ ...toDoAdd, content: "" });
+  };
+
+  // 일정 추가 Input
   const toDoInputFunc = (e) => {
     const { name, value } = e.target;
     setToDoAdd({ ...toDoAdd, content: value });
   };
 
-  // toDo 등록
+  // toDo 등록 버튼
   const toDoAddFunc = (e) => {
     e.preventDefault();
     columns["ready"].items.push({
       id: uuid(),
       content: toDoAdd.content,
       condition: "ready",
+      index: columns["ready"].items.length,
     });
-    console.log(toDoAdd.content);
+    console.log(columns["ready"].items);
 
     setIsClick((prev) => !prev);
-  };
-
-  // toDo 추가 및 취소
-  const onClickAddOpen = () => {
-    setIsClick((prev) => !prev);
-    setToDoAdd({ ...toDoAdd, content: "" });
   };
 
   // toDo 삭제
@@ -121,11 +171,21 @@ function RoomDetail() {
     if (window.confirm("정말로 삭제하시겠습니까?")) {
       const { id, value } = e.target;
 
-      console.log(columns[id].name);
-      // const removeArr = columns[id].items;
+      let sortIndex = 0;
 
       // filter를 통해 내가 삭제를 원하는 item을 빼고 새 배열을 생성
-      const updateArr = columns[id].items.filter((delId) => delId.id !== value);
+      const updateArr = columns[id].items.filter((delId) => {
+        if (delId.id === value) {
+          sortIndex = delId.index;
+        }
+        return delId.id !== value;
+      });
+
+      updateArr.forEach((element) => {
+        if (element.index > sortIndex) {
+          element.index -= 1;
+        }
+      });
 
       setColumns({
         ...columns,
