3f4a9b37652efd12bb3a1f483395eae3078c8186
spiky-haired-Pyramid-head <87762815+spiky-haired-Pyramid-head@users.noreply.github.com>
2022-05-06 18:17:50 +0900

    User&Room Controller 구현

--- src/main/java/Wap/Todo/controller/RoomController.java
+++ src/main/java/Wap/Todo/controller/RoomController.java
@@ -1,6 +1,7 @@
 package Wap.Todo.controller;
 
 import Wap.Todo.domain.Room;
+import Wap.Todo.domain.Todo;
 import Wap.Todo.service.RoomService;
 import Wap.Todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
 @Controller
@@ -17,6 +19,7 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    //방삭제
     @DeleteMapping("/{room_num}")
     @ResponseBody
     public Long deleteRoom(@PathVariable("room_num") Long num, HttpSession session){
@@ -27,6 +30,7 @@ public class RoomController {
         return room_num;
     }
 
+    //방만들기
     @PostMapping("/invite")
     @ResponseBody
     public String createRoom(@RequestBody String introduce, HttpSession session){
@@ -35,6 +39,47 @@ public class RoomController {
         Room room = roomService.joinRoom(introduce,id);
 
         return room.toString();
+    }
+
+    //방 초대받기
+    @GetMapping("/{invite_code}")
+    @ResponseBody
+    public String inviteRoom(@PathVariable("invite_code") String code, HttpSession session){
+
+
+        String id = (String)session.getAttribute("memberId");
+        Room room = roomService.attend(code, id);
+        return room.toString();
+    }
+
+    //방입장
+    @GetMapping("/{room_num}")
+    @ResponseBody
+    public List<Todo> enterRoom(@PathVariable("room_num") Long num){
+
+        List<Todo> todos = roomService.getTodos(num);
+        return todos;
+    }
+
+    //투두 등록 및 수정
+    @PostMapping("/{room_num}/todo")
+    @ResponseBody
+    public String modifyTodo(@PathVariable("room_num") Long num,
+                             @RequestBody Todo todo, HttpSession session){
+        String id = (String)session.getAttribute("memberId");
+
+        Todo updatetodo = roomService.updateTodo(num, todo, id);
+        return updatetodo.toString();
+    }
+
+    //투두 삭제
+    @DeleteMapping("/{room_num}/{id}")
+    @ResponseBody
+    public String deletetodo(@PathVariable("room_num") Long num,
+                           @PathVariable ("id") Long id){
+        Todo todo = roomService.deleteTodo(num, id);
+
+        return todo.toString();
     }
 
 
