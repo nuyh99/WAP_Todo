75dd43639bd5b29f93c64c10e54be6deeda4d263
spiky-haired-Pyramid-head <qns0147@gmail.com>
2022-05-15 20:42:16 +0900

    allowMethods 설정 및 api 주소 변환

--- src/main/java/Wap/Todo/controller/WebConfig.java
+++ src/main/java/Wap/Todo/controller/WebConfig.java
@@ -1,6 +1,7 @@
 package Wap.Todo.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
                 .allowCredentials(true);
     }
 }
