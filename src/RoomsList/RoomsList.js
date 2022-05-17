bb6b137781c8c3abdd938002056f1245d5a3ad99
Jeong Seok <wjdtjr8649@naver.com>
2022-05-17 23:12:30 +0900

    05-17 Update Delete Room

--- src/RoomsList/RoomsList.js
+++ src/RoomsList/RoomsList.js
@@ -1,9 +1,43 @@
-import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { UserDispatch } from "./PersonalRooms";
+import axios from "axios";
+
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Button } from "@mui/material";
 
 const RoomsList = (prop) => {
+  axios.defaults.withCredentials = true;
+
   console.log(prop);
+
+  const onClickDelete = (e, detail) => {
+    e.preventDefault();
+
+    const inputMsg = prompt(
+      `방 삭제를 원하시면 방 이름(${detail.title})을 정확하게 입력하세요.`
+    );
+    if (inputMsg === detail.title) {
+      console.log("일치합니다.");
+      deleteRoom(detail.num);
+    } else {
+      console.log("방 이름이 일치하지 않습니다. 다시 입력하세요.");
+      window.alert("방 이름을 정확하게 입력해주세요.");
+    }
+
+    console.log(e, detail);
+  };
+
+  // 방 이름을 정확히 입력했을 때
+  const deleteRoom = async (num) => {
+    axios.defaults.withCredentials = true;
+    console.log(num);
+
+    const res = await axios({
+      method: "delete",
+      url: `http://localhost:8080/room/${num}`,
+    });
+    window.alert("방 삭제가 완료되었습니다.");
+    window.location.replace("/");
+  };
 
   return (
     <div>
@@ -28,6 +62,13 @@ const RoomsList = (prop) => {
                 방 이름 : {detail.title}
                 <br />방 한줄 소개 : {detail.introduce}
               </Link>
+              <Button
+                onClick={(e) => onClickDelete(e, detail)}
+                endIcon={<DeleteForeverIcon />}
+                style={{ color: "red" }}
+              >
+                삭제
+              </Button>
             </div>
           );
         })}
