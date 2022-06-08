8b2be4b7f16aeade00647ce54258ff593932cf1c
jaehyun hwang <93072571+nuyh99@users.noreply.github.com>
2022-06-08 11:01:08 +0900

    revive test code

--- src/test/java/Wap/Todo/service/RoomServiceTest.java
+++ src/test/java/Wap/Todo/service/RoomServiceTest.java
@@ -1,90 +1,90 @@
-//package Wap.Todo.service;
-//
-//import Wap.Todo.domain.*;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.security.NoSuchAlgorithmException;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//@SpringBootTest
-//class RoomServiceTest {
-//    private final UserService userService;
-//    private final RoomService roomService;
-//    private final RoomRepository roomRepository;
-//    private final MemberRepository memberRepository;
-//
-//    @Autowired
-//    RoomServiceTest(UserService userService, RoomService roomService, RoomRepository roomRepository, MemberRepository memberRepository) {
-//        this.userService = userService;
-//        this.roomService = roomService;
-//        this.roomRepository = roomRepository;
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("방 삭제")
-//    public void deleteRoom() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("id", "pw", "name", null, null);
-//        member = userService.join(member);        //회원가입
-//
-//        Room room = roomService.joinRoom("test","제목", member.getId());
-//        System.out.println(room);               //방 생성
-//
-//        //when
-//        Long id = roomService.deleteRoom(room.getNum(), member.getId());
-//
-//        //then
-//        assertThat(id).isNotNull();
-//        assertThat(roomService.deleteRoom(room.getNum() + 1L, member.getId())).isNull();
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("방의 마스터가 해당 방을 삭제했을 때 나머지 유저들 CASCADE")
-//    public void cascadeTest() throws NoSuchAlgorithmException {
-//        Member member = new Member("id", "pw2", "name", null, null);
-//        member = userService.join(member);        //회원가입
-//        Member member2 = new Member("id2", "pw", "name", null, null);
-//        member2 = userService.join(member2);        //회원가입
-//
-//        Room room = roomService.joinRoom("test","", member.getId());
-//        System.out.println(room);               //방 생성
-//        roomService.attend(room.getCode(), member2.getId());
-//
-//        //when
-//        roomService.deleteRoom(room.getNum(), member.getId());
-//
-//        //then
-//        assertThat(memberRepository.getById(member2.getId()).getRooms().contains(room)).isFalse();
-//        System.out.println(member2.getRooms());
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("방 초대 받기")
-//    public void invitingUser() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("id", "pw2", "name", null, null);
-//        member = userService.join(member);        //회원가입
-//        Member member2 = new Member("id2", "pw", "name", null, null);
-//        member2 = userService.join(member2);        //회원가입
-//
-//        Room room = roomService.joinRoom("test","", member.getId());
-//        System.out.println(room);               //방 생성
-//
-//
-//        //when
-//        Room attend = roomService.attend(room.getCode(), member2.getId());
-//
-//        //then
-//        assertThat(attend).isNotNull();
-//        assertThat(member2.getRooms().contains(attend)).isTrue();
-//    }
-//}
+package Wap.Todo.service;
+
+import Wap.Todo.domain.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class RoomServiceTest {
+    private final UserService userService;
+    private final RoomService roomService;
+    private final RoomRepository roomRepository;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    RoomServiceTest(UserService userService, RoomService roomService, RoomRepository roomRepository, MemberRepository memberRepository) {
+        this.userService = userService;
+        this.roomService = roomService;
+        this.roomRepository = roomRepository;
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("방 삭제")
+    public void deleteRoom() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("id", "pw", "name", null, null);
+        member = userService.join(member);        //회원가입
+
+        Room room = roomService.joinRoom("test","제목", member.getId());
+        System.out.println(room);               //방 생성
+
+        //when
+        Long id = roomService.deleteRoom(room.getNum(), member.getId());
+
+        //then
+        assertThat(id).isNotNull();
+        assertThat(roomService.deleteRoom(room.getNum() + 1L, member.getId())).isNull();
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("방의 마스터가 해당 방을 삭제했을 때 나머지 유저들 CASCADE")
+    public void cascadeTest() throws NoSuchAlgorithmException {
+        Member member = new Member("id", "pw2", "name", null, null);
+        member = userService.join(member);        //회원가입
+        Member member2 = new Member("id2", "pw", "name", null, null);
+        member2 = userService.join(member2);        //회원가입
+
+        Room room = roomService.joinRoom("test","", member.getId());
+        System.out.println(room);               //방 생성
+        roomService.attend(room.getCode(), member2.getId());
+
+        //when
+        roomService.deleteRoom(room.getNum(), member.getId());
+
+        //then
+        assertThat(memberRepository.getById(member2.getId()).getRooms().contains(room)).isFalse();
+        System.out.println(member2.getRooms());
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("방 초대 받기")
+    public void invitingUser() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("id", "pw2", "name", null, null);
+        member = userService.join(member);        //회원가입
+        Member member2 = new Member("id2", "pw", "name", null, null);
+        member2 = userService.join(member2);        //회원가입
+
+        Room room = roomService.joinRoom("test","", member.getId());
+        System.out.println(room);               //방 생성
+
+
+        //when
+        Room attend = roomService.attend(room.getCode(), member2.getId());
+
+        //then
+        assertThat(attend).isNotNull();
+        assertThat(member2.getRooms().contains(attend)).isTrue();
+    }
+}
--- src/test/java/Wap/Todo/service/UserServiceTest.java
+++ src/test/java/Wap/Todo/service/UserServiceTest.java
@@ -1,94 +1,94 @@
-//package Wap.Todo.service;
-//
-//import Wap.Todo.domain.Member;
-//import Wap.Todo.domain.MemberRepository;
-//import Wap.Todo.domain.Room;
-//import Wap.Todo.domain.RoomRepository;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.security.NoSuchAlgorithmException;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//class UserServiceTest {
-//    private final UserService userService;
-//    private final MemberRepository memberRepository;
-//    private final RoomRepository roomRepository;
-//
-//    @Autowired
-//    public UserServiceTest(MemberRepository memberRepository, RoomRepository roomRepository, UserService userService) {
-//        this.userService = userService;
-//        this.memberRepository = memberRepository;
-//        this.roomRepository = roomRepository;
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("회원가입")
-//    public void join() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("test", "123", "헤헤", null,null);
-//
-//        //when
-//        member=userService.join(member);
-//
-//        //then
-//        assertThat(memberRepository.findById(member.getId()).get().getId()).isEqualTo("test");
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("중복 회원가입")
-//    public void duplicateJoin() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("testabcd", "123", "헤헤", null,null);
-//        Member member2 = new Member("testabcd", "123", "헤헤", null,null);
-//
-//        //when
-//        userService.join(member);
-//
-//        //then
-//        assertThat(userService.join(member2)).isNull();
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("로그인")
-//    public void login() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("test", "123", "헤헤",null, null);
-//        Member toLogin = new Member("test", "123", null,null, null);
-//        Member toLogin2 = new Member("test", "1234", null,null, null);
-//
-//        //when
-//        userService.join(member);
-//
-//        //then
-//        assertThat(userService.login(toLogin)).isNotNull();
-//        assertThat(userService.login(toLogin2)).isNull();
-//        System.out.println(userService.login(toLogin).toString());
-//    }
-//
-//    @Transactional
-//    @Test
-//    @DisplayName("방 목록 가져오기")
-//    public void getRooms() throws NoSuchAlgorithmException {
-//        //given
-//        Member member = new Member("test", "123", "헤헤", null,null);
-//        Room room=new Room();
-//        room.setNum(5L);
-//
-//        //when
-//        userService.join(member);
-//        roomRepository.save(room);
-//        memberRepository.getById(member.getId()).getRooms().add(room);
-//
-//        //then
-//        assertThat(userService.getRoomsById(member.getId()).get(0).getNum()).isEqualTo(5L);
-//    }
-//}
+package Wap.Todo.service;
+
+import Wap.Todo.domain.Member;
+import Wap.Todo.domain.MemberRepository;
+import Wap.Todo.domain.Room;
+import Wap.Todo.domain.RoomRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UserServiceTest {
+    private final UserService userService;
+    private final MemberRepository memberRepository;
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public UserServiceTest(MemberRepository memberRepository, RoomRepository roomRepository, UserService userService) {
+        this.userService = userService;
+        this.memberRepository = memberRepository;
+        this.roomRepository = roomRepository;
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("회원가입")
+    public void join() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("test", "123", "헤헤", null,null);
+
+        //when
+        member=userService.join(member);
+
+        //then
+        assertThat(memberRepository.findById(member.getId()).get().getId()).isEqualTo("test");
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("중복 회원가입")
+    public void duplicateJoin() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("testabcd", "123", "헤헤", null,null);
+        Member member2 = new Member("testabcd", "123", "헤헤", null,null);
+
+        //when
+        userService.join(member);
+
+        //then
+        assertThat(userService.join(member2)).isNull();
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("로그인")
+    public void login() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("test", "123", "헤헤",null, null);
+        Member toLogin = new Member("test", "123", null,null, null);
+        Member toLogin2 = new Member("test", "1234", null,null, null);
+
+        //when
+        userService.join(member);
+
+        //then
+        assertThat(userService.login(toLogin)).isNotNull();
+        assertThat(userService.login(toLogin2)).isNull();
+        System.out.println(userService.login(toLogin).toString());
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("방 목록 가져오기")
+    public void getRooms() throws NoSuchAlgorithmException {
+        //given
+        Member member = new Member("test", "123", "헤헤", null,null);
+        Room room=new Room();
+        room.setNum(5L);
+
+        //when
+        userService.join(member);
+        roomRepository.save(room);
+        memberRepository.getById(member.getId()).getRooms().add(room);
+
+        //then
+        assertThat(userService.getRoomsById(member.getId()).get(0).getNum()).isEqualTo(5L);
+    }
+}
